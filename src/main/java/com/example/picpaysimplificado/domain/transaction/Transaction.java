9e02bb272bbb1c46a417fa633518238b818069a8
Alefe Alves <alefealves@hotmail.com>
2024-04-03 09:44:39 -0400

    initial commit

---
+++ src/main/java/com/example/picpaysimplificado/domain/transaction/Transaction.java
@@ -0,0 +1,69 @@
+package com.example.picpaysimplificado.domain.transaction;
+
+import com.example.picpaysimplificado.domain.user.User;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity(name="transactions")
+@Table(name="transactions")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(of="id")
+public class Transaction {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private BigDecimal amount;
+    @ManyToOne
+    @JoinColumn(name="sender_id")
+    private User sender;
+    @ManyToOne
+    @JoinColumn(name="receiver_id")
+    private User receiver;
+    private LocalDateTime timestamp;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public BigDecimal getAmount() {
+        return amount;
+    }
+
+    public void setAmount(BigDecimal amount) {
+        this.amount = amount;
+    }
+
+    public User getSender() {
+        return sender;
+    }
+
+    public void setSender(User sender) {
+        this.sender = sender;
+    }
+
+    public User getReceiver() {
+        return receiver;
+    }
+
+    public void setReceiver(User receiver) {
+        this.receiver = receiver;
+    }
+
+    public LocalDateTime getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(LocalDateTime timestamp) {
+        this.timestamp = timestamp;
+    }
+}
